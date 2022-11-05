032e6b768a0e20b73b78340ea06b4d5e0f6113b2
dmitrij-pularija <dmitrij.pularija@gmail.com>
2022-11-05 14:59:32 +0200

    hw9

--- js/03-promises.js
+++ js/03-promises.js
@@ -1,15 +1,18 @@
 import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 const form = document.querySelector(".form");
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   const objPromise = { position, delay };
   return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      resolve(objPromise);
-    } else {
-      reject(objPromise);
-    }
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(objPromise);
+      } else {
+        reject(objPromise);
+      }
+    }, delay);
   });
 }
 
@@ -23,24 +26,21 @@ function submit(event) {
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        const timerId = setTimeout(() => {
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
-            useIcon: false,
-            cssAnimationStyle: "zoom",
-            timeout: 1000,
-          });
-        }, delay);
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+          useIcon: false,
+          cssAnimationStyle: "zoom",
+          timeout: 1000,
+        });
       })
       .catch(({ position, delay }) => {
-        const timerId = setTimeout(() => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
-            useIcon: false,
-            cssAnimationStyle: "zoom",
-            timeout: 1000,
-          });
-        }, delay);
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+          useIcon: false,
+          cssAnimationStyle: "zoom",
+          timeout: 1000,
+        });
       });
     delay += step;
   }
 }
+
 form.addEventListener("submit", submit);
